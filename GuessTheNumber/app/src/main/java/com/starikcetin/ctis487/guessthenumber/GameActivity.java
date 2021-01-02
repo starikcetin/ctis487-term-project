508a1f4d333b5031796c940c42a103afb6ca125a
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-01-02 18:09:22 +0300

    view pager is hooked-up and working

--- GuessTheNumber/app/src/main/java/com/starikcetin/ctis487/guessthenumber/GameActivity.java
+++ GuessTheNumber/app/src/main/java/com/starikcetin/ctis487/guessthenumber/GameActivity.java
@@ -1,14 +1,19 @@
 package com.starikcetin.ctis487.guessthenumber;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class GameActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ViewPager2 pager = (ViewPager2) findViewById(R.id.game_body_viewPager_static);
+        pager.setAdapter(new GamePagerAdapter(this));
+        pager.setCurrentItem(GamePagerAdapter.GUESSING_PAGE_INDEX, false);
     }
 }
---
+++ GuessTheNumber/app/src/main/java/com/starikcetin/ctis487/guessthenumber/GamePagerAdapter.java
@@ -0,0 +1,34 @@
+package com.starikcetin.ctis487.guessthenumber;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class GamePagerAdapter extends FragmentStateAdapter {
+    public static final int PAGE_COUNT = 3;
+    public static final int OPTIONS_PAGE_INDEX = 0;
+    public static final int GUESSING_PAGE_INDEX = 1;
+    public static final int PREV_GUESSES_PAGE_INDEX = 2;
+
+    public GamePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case OPTIONS_PAGE_INDEX: return new GameOptionsFragment();
+            case GUESSING_PAGE_INDEX: return new GameGuessingFragment();
+            case PREV_GUESSES_PAGE_INDEX: return new GamePrevGuessesFragment();
+        }
+
+        throw new IndexOutOfBoundsException("GamePager: position " + position + " is invalid.");
+    }
+
+    @Override
+    public int getItemCount() {
+        return PAGE_COUNT;
+    }
+}
